758fa35bda7c9c8a2fa96f743b0414ba650d9844
Honam Kim <tigerrla@naver.com>
2025-11-05 14:11:14 +0900

    create project

---
+++ src/main/java/com/ll/gooream/GooreamApplication.java
@@ -0,0 +1,13 @@
+package com.ll.gooream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GooreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GooreamApplication.class, args);
+    }
+
+}
