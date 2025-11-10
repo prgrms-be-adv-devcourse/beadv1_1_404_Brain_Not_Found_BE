856c30eb2b369eb4afcdc9182fc96ca6001b049a
Honam Kim <tigerrla@naver.com>
2025-11-10 11:43:52 +0900

    [feat][7] 주문 목록 조회, 상세 조회, 검색 api 설계 (#10)

--- order/src/main/java/com/ll/order/domain/controller/OrderController.java
+++ order/src/main/java/com/ll/order/domain/controller/OrderController.java
@@ -1,50 +1,48 @@
 package com.ll.order.domain.controller;
 
-import com.ll.order.domain.model.entity.Order;
 import com.ll.order.domain.model.vo.request.OrderCartItemRequest;
 import com.ll.order.domain.model.vo.request.OrderDirectRequest;
+import com.ll.order.domain.model.vo.response.OrderCreateResponse;
+import com.ll.order.domain.model.vo.response.OrderDetailResponse;
+import com.ll.order.domain.model.vo.response.OrderPageResponse;
 import com.ll.order.domain.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/orders")
 @RequiredArgsConstructor
 public class OrderController {
+    private final OrderService orderService;
+
     /*
     주문 생성 : 클라이언트 주문 생성 및 DB 저장
     → 결제 요청 : 주문 서비스에서 결제 서비스에 요청 및 응답 대기
     → 결제 처리 : 주문의 요청을 받아 처리. 외부 API 연동
     → 주문 상태 업데이트 : 결제 결과에 따라 주문 상태 변경 (created → paid / failed)
     * */
-
-    private final OrderService orderService;
-
     @PostMapping("/cartItems")
-    public ResponseEntity<Order> createCartItemOrder(
+    public ResponseEntity<OrderCreateResponse> createCartItemOrder(
             @RequestBody OrderCartItemRequest request
     ) {
-        Order cartItemOrder = orderService.createCartItemOrder(request);
+        OrderCreateResponse response = orderService.createCartItemOrder(request);
 
         return ResponseEntity
                 .status(HttpStatus.CREATED)
-                .body(cartItemOrder);
+                .body(response);
     }
 
     @PostMapping("/direct")
-    public ResponseEntity<Order> createDirectOrder(
+    public ResponseEntity<OrderCreateResponse> createDirectOrder(
             @RequestBody OrderDirectRequest request
     ) {
-        Order directOrder = orderService.createDirectOrder(request);
+        OrderCreateResponse response = orderService.createDirectOrder(request);
 
         return ResponseEntity
                 .status(HttpStatus.CREATED)
-                .body(directOrder);
+                .body(response);
     }
 
 //    @DeleteMapping("/{orderCode}")
@@ -56,17 +54,32 @@ public class OrderController {
 //        return ResponseEntity.ok(null);
 //    }
 //
-//    @GetMapping
-//    public ResponseEntity<List<OrderListApiResponse>> getOrderList(
-//            @RequestParam String userCode,
-//            @RequestParam int page, @RequestParam int size, @RequestParam String sort
-//    ) {
-//        List<OrderListApiResponse> allOrders = orderService.findAllOrders(userCode, page, size, sort);
-//
-//        return ResponseEntity
-//                .status(HttpStatus.OK)
-//                .body(allOrders);
-//    }
+    @GetMapping
+    public ResponseEntity<OrderPageResponse> getOrderList(
+            @RequestParam String userCode,
+            @RequestParam(required = false) String keyword,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "id") String sortBy,
+            @RequestParam(defaultValue = "desc") String sortOrder
+    ) {
+        OrderPageResponse orderPageResponse = orderService.findAllOrders(userCode, keyword, page, size, sortBy, sortOrder);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(orderPageResponse);
+    }
+
+    @GetMapping("/{orderCode}/details")
+    public ResponseEntity<OrderDetailResponse> getOrderDetails(
+            @PathVariable String orderCode
+    ) {
+        OrderDetailResponse orderDetails = orderService.findOrderDetails(orderCode);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(orderDetails);
+    }
 //
 //    @PostMapping
 //    public ResponseEntity getPaymentRequest(
@@ -76,16 +89,7 @@ public class OrderController {
 //        return ResponseEntity.ok(null);
 //    }
 //
-//    @GetMapping("/{orderCode}/details")
-//    public ResponseEntity<OrderDetailResponse> getOrderDetails(
-//            @RequestParam String orderCode
-//    ) {
-//        OrderDetailResponse orderDetails = orderService.findOrderDetails(orderCode);
-//
-//        return ResponseEntity
-//                .status(HttpStatus.OK)
-//                .body(orderDetails);
-//    }
+
 //
 //    @PatchMapping("/{orderCode}/status")
 //    public ResponseEntity updateOrderStatus(
@@ -113,15 +117,6 @@ public class OrderController {
 //        return ResponseEntity.ok(null);
 //    }
 //
-//    @GetMapping("/search")
-//    public ResponseEntity searchOrders(
-//            @RequestParam String userCode,
-//            @RequestParam String searchType, @RequestParam String searchValue,
-//            @RequestParam int page, @RequestParam int size, @RequestParam String sort
-//    ) {
-//
-//        return ResponseEntity.ok(null);
-//    }
 //
 //    @PostMapping("/{orderCode}/complete")
 //    public ResponseEntity completeOrder(
