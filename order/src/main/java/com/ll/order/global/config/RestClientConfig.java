17c76e8f0e83a13a0ffeeaf6118ba4dbfbe2a8c5
Honam Kim <tigerrla@naver.com>
2025-11-11 14:04:25 +0900

    [feat][12] 주문-결제 로직 구현. 예치금 관련 로직은 주석처리
    # Conflicts:
    #	settings.gradle
    
    # Conflicts:
    #	order/build.gradle
    #	order/src/main/java/com/ll/order/domain/client/PaymentServiceClient.java
    #	order/src/main/java/com/ll/order/domain/model/vo/request/OrderCartItemRequest.java
    #	order/src/main/java/com/ll/order/domain/model/vo/request/OrderDirectRequest.java
    #	order/src/main/java/com/ll/order/domain/service/OrderServiceImpl.java
    #	payment/src/main/java/com/ll/payment/controller/PaymentController.java
    #	payment/src/main/java/com/ll/payment/model/entity/Payment.java
    #	payment/src/main/java/com/ll/payment/service/PaymentServiceImpl.java

--- order/src/main/java/com/ll/order/global/config/RestClientConfig.java
+++ order/src/main/java/com/ll/order/global/config/RestClientConfig.java
@@ -7,6 +7,7 @@ import org.springframework.web.client.RestClient;
 @Configuration
 public class RestClientConfig {
 
+    //RestTemplate 보다 고도화된 RestClient 추천드립니다.
     @Bean
     public RestClient restClient(RestClient.Builder builder) {
         return builder.build();
