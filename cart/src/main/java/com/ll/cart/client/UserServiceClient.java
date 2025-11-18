7eb112d3d6177e8cda80f67d9254f117b57e9516
Honam Kim <tigerrla@naver.com>
2025-11-18 15:12:06 +0900

    refactor/55 cart 모듈 response 정리 - 타 모듈과 중복되는 부분 있었음. 이에 맞게 테스트 코드 수정

--- cart/src/main/java/com/ll/cart/client/UserServiceClient.java
+++ cart/src/main/java/com/ll/cart/client/UserServiceClient.java
@@ -1,13 +1,18 @@
 package com.ll.cart.client;
 
-import com.ll.cart.model.vo.response.UserResponse;
 import com.ll.core.model.response.BaseResponse;
+import com.ll.user.model.enums.AccountStatus;
+import com.ll.user.model.enums.Grade;
+import com.ll.user.model.enums.Role;
+import com.ll.user.model.enums.SocialProvider;
+import com.ll.user.model.vo.response.UserResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
+import java.time.LocalDateTime;
 import java.util.Map;
 
 @Component
@@ -24,7 +29,7 @@ public class UserServiceClient {
                 .uri(userServiceUrl + "/api/users/info")
                 .header("X-User-Code", userCode)
                 .retrieve()
-                .body(new ParameterizedTypeReference<BaseResponse<Map<String, Object>>>() {});
+                .body(new ParameterizedTypeReference<>() {});
         
         if (response == null || response.getData() == null) {
             return null;
@@ -33,20 +38,20 @@ public class UserServiceClient {
         Map<String, Object> userData = response.getData();
         return new UserResponse(
                 userData.get("id") != null ? Long.valueOf(userData.get("id").toString()) : null,
+                userData.get("code") != null ? userData.get("code").toString() : null,
                 userData.get("socialId") != null ? userData.get("socialId").toString() : null,
-                userData.get("socialProvider") != null ? com.ll.cart.model.enums.SocialProvider.valueOf(userData.get("socialProvider").toString()) : null,
+                userData.get("socialProvider") != null ? SocialProvider.valueOf(userData.get("socialProvider").toString()) : null,
                 userData.get("email") != null ? userData.get("email").toString() : null,
                 userData.get("name") != null ? userData.get("name").toString() : null,
-                userData.get("role") != null ? com.ll.cart.model.enums.Role.valueOf(userData.get("role").toString()) : null,
+                userData.get("role") != null ? Role.valueOf(userData.get("role").toString()) : null,
                 userData.get("profileImageUrl") != null ? userData.get("profileImageUrl").toString() : null,
                 userData.get("mannerScore") != null ? Long.valueOf(userData.get("mannerScore").toString()) : null,
-                userData.get("grade") != null ? com.ll.cart.model.enums.Grade.valueOf(userData.get("grade").toString()) : null,
-                userData.get("accountStatus") != null ? com.ll.cart.model.enums.AccountStatus.valueOf(userData.get("accountStatus").toString()) : null,
+                userData.get("grade") != null ? Grade.valueOf(userData.get("grade").toString()) : null,
+                userData.get("accountStatus") != null ? AccountStatus.valueOf(userData.get("accountStatus").toString()) : null,
                 userData.get("accountBank") != null ? userData.get("accountBank").toString() : null,
                 userData.get("accountNumber") != null ? userData.get("accountNumber").toString() : null,
-                userData.get("address") != null ? userData.get("address").toString() : null,
-                userData.get("createAt") != null ? java.time.LocalDateTime.parse(userData.get("createAt").toString()) : null,
-                userData.get("updatedAt") != null ? java.time.LocalDateTime.parse(userData.get("updatedAt").toString()) : null
+                userData.get("createAt") != null ? LocalDateTime.parse(userData.get("createAt").toString()) : null,
+                userData.get("updatedAt") != null ? LocalDateTime.parse(userData.get("updatedAt").toString()) : null
         );
     }
 }
