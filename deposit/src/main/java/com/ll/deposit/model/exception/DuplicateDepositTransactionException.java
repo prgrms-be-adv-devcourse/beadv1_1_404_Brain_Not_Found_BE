e66f880568f618422e2a7564ff86323155c66704
Dianuma <dltmdgus1208@gmail.com>
2025-11-26 14:09:24 +0900

    - BaseErrorCode 생성 - ErrorCode 모듈 별로 분리

--- deposit/src/main/java/com/ll/deposit/model/exception/DuplicateDepositTransactionException.java
+++ deposit/src/main/java/com/ll/deposit/model/exception/DuplicateDepositTransactionException.java
@@ -1,7 +1,6 @@
 package com.ll.deposit.model.exception;
 
 import com.ll.core.model.exception.BaseException;
-import com.ll.core.model.exception.ErrorCode;
 
 public class DuplicateDepositTransactionException extends BaseException {
 
