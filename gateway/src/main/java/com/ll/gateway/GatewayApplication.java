6706aef14e72311197e312b8fab0addb07a2068b
YeSeong <94231335+yuio7279@users.noreply.github.com>
2025-11-26 11:56:08 +0900

    [fix][72] 로컬 환경에서 실행 되지 않는 문제 해결 (#73)
    * [fix][72] 전체 모듈 application.yml 수정
    
    * [fix][72] .gitignore내용 추가
    
    * [fix][72] 모든 모듈  application-local.yml 추가
    
    * [chore][72] 모든 모듈 build.gradle 문서정리
    
    * [fix][72] 모든 모듈 application-dev 추가
    
    * [fix][72] 모든 모듈 application-prod 추가
    
    * [fix][72] 카테고리 엔티티 테이블 명 컨벤션 맞게 수정
    
    * [feat][72] 로컬 실행용 카프카 추가 core/
    
    * [feat][72] 유레카 서비스 등록 어노테이션 추가
    
    * [chore][72] 불필요한 설정 제거
    
    * [chore][72] 도커 환경 복잡한 헬스체크 임시 제거
    
    * 로컬 실행 안되던 문제 및 환경분리 안내사항
    
    * Update README_1122.md
    
    * Update README_1122.md
    
    * Update README_1122.md
    
    * Update README_1122.md

--- gateway/src/main/java/com/ll/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/ll/gateway/GatewayApplication.java
@@ -4,10 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication(exclude = {
-        org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
-        org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class
-})
+@SpringBootApplication
 @EnableDiscoveryClient
 public class GatewayApplication {
     public static void main(String[] args) {
