2b73c6792116eadfbf298e82bd73eb77d4176887
yuio7279 <yuio7279@gmail.com>
2025-12-03 11:14:55 +0900

    [refactor][119] 서비스 별 Application.java 디스커버리 어노테이션 제거

--- order/src/main/java/com/ll/order/OrderApplication.java
+++ order/src/main/java/com/ll/order/OrderApplication.java
@@ -2,14 +2,12 @@ package com.ll.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@EnableDiscoveryClient
 @EnableRetry
 @EnableScheduling  // 스케줄러 활성화
 public class OrderApplication {
