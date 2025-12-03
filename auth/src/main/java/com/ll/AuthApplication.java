5ec345680ecf6a79a8a971b520d69a54f03ad59b
YeSeong <94231335+yuio7279@users.noreply.github.com>
2025-12-03 12:08:19 +0900

    [refactor][119] 쿠버네티스 환경 구성 - 3  (#131)
    * [refactor][119] 서비스 별 application-dev.yml 수정
    
    * [refactor][119] 민감파일 secret-*-*.yaml git.ignore 추가
    
    * [refactor][119] 서비스별 build.gradle 변경[디스커버리 의존성 제거]
    
    * [refactor][119] 서비스 별 Application.java 디스커버리 어노테이션 제거
    
    * [refactor][119] 테스트용 ping api 제거
    
    * [refactor][119] k8s-global 디렉토리(dev 환경) 추가
    기존 : 각 서비스/k8s/dev/*.yaml
    현재 : k8s-global/dev/external, outer, services/*.yaml로 변경
    
    * [refactor][119] settings.gradle에 user 하위모듈 설정 제거
    
    * [refactor][119] 서비스 별 Dockerfile jar 생성 임시 주석처리 (dev용)
    
    * [feat][119] 게이트웨이 토큰에서 유저정보 추출 추가
    
    * [feat][119] 쿠버네티스 dev용 자동 구성 배치파일 추가
    
    ---------
    
    Co-authored-by: LEE Seunghyun <83849667+Dianuma@users.noreply.github.com>

--- auth/src/main/java/com/ll/AuthApplication.java
+++ auth/src/main/java/com/ll/AuthApplication.java
@@ -2,11 +2,9 @@ package com.ll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableJpaAuditing
 public class AuthApplication {
 
