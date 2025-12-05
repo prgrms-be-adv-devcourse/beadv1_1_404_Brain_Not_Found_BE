8906fc1dd8a8db3ba9e1b9ac87a876020e94b083
98minseok <als981209@gmail.com>
2025-12-05 17:02:55 +0900

    [feat][115] Scheduler 오류 수정

--- products/src/main/java/com/ll/products/domain/history/repository/HistoryProcedureRepository.java
+++ products/src/main/java/com/ll/products/domain/history/repository/HistoryProcedureRepository.java
@@ -3,6 +3,7 @@ package com.ll.products.domain.history.repository;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 @RequiredArgsConstructor
@@ -10,10 +11,12 @@ public class HistoryProcedureRepository {
 
     private final EntityManager em;
 
+    @Transactional
     public void cleanViewHistory() {
         em.createNativeQuery("CALL clean_view_history()").executeUpdate();
     }
 
+    @Transactional
     public void cleanSearchHistory() {
         em.createNativeQuery("CALL clean_search_history()").executeUpdate();
     }
