c99c3e31261bdc51dfec95e65e46b75715fa5ddf
98minseok <als981209@gmail.com>
2025-12-09 09:31:10 +0900

    [feat][163] Swagger 기본설정

--- auth/src/main/java/com/ll/auth/controller/AuthController.java
+++ auth/src/main/java/com/ll/auth/controller/AuthController.java
@@ -4,11 +4,14 @@ import com.ll.core.model.response.BaseResponse;
 import com.ll.auth.model.vo.dto.Tokens;
 import com.ll.auth.model.vo.request.TokenValidRequest;
 import com.ll.auth.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name ="Auth", description = "토큰 관리 API")
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
@@ -17,6 +20,9 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping
+    @Operation(summary = "토큰 재발급" ,
+            description =
+                    "Cookie 에 저장된 refreshToken, deviceCode 값을 통해 토큰을 재발급하여 쿠키에 저장합니다.")
     public ResponseEntity<BaseResponse<Tokens>> refreshToken(
             @CookieValue(name = "refreshToken", required = false) String refreshToken,
             @CookieValue(name = "deviceCode", required = false) String deviceCode,
@@ -29,6 +35,9 @@ public class AuthController {
     }
 
     @PostMapping("/logout")
+    @Operation(summary = "로그아웃" , description =
+            "Cookie 에 저장된 refreshToken, deviceCode 값을 통해 토큰 정보를 Redis 및 DB에서 삭제하고, 쿠키에서 만료시킵니다.")
+
     public ResponseEntity<Void> logout(
             @CookieValue(name = "refreshToken") String refreshToken,
             @CookieValue(name ="deviceCode", required = false) String deviceCode,
