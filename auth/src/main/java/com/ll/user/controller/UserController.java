8c311de371fcce8a91086b03995bfb85166d29b0
Kim minseok <80530044+98minseok@users.noreply.github.com>
2025-12-09 14:38:09 +0900

    [feat][163] Swagger 적용 (#166)
    * [feat][163] Swagger 기본설정
    
    * [feat][163] Swagger 게이트웨이 설정
    
    * [feat][163] Swagger Payment,Order 설정
    
    * [fix][169] 이전 버전 implementation 삭제

--- auth/src/main/java/com/ll/user/controller/UserController.java
+++ auth/src/main/java/com/ll/user/controller/UserController.java
@@ -9,6 +9,8 @@ import com.ll.common.model.vo.response.UserLoginResponse;
 import com.ll.user.model.vo.response.UserResponse;
 import com.ll.user.service.UserService;
 import com.ll.core.model.response.BaseResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +18,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
+@Tag(name = "User", description = "사용자 관련 API")
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
@@ -24,6 +27,7 @@ public class UserController {
     private final AuthService authService;
 
     // 회원 정보 조회
+    @Operation(summary = "회원 정보 조회" , description = "UserCode로 회원 정보를 조회합니다.")
     @GetMapping("/info")
     public ResponseEntity<BaseResponse<UserResponse>> getUser(
             @RequestHeader(value = "X-User-Code") String userCode
@@ -32,6 +36,7 @@ public class UserController {
     }
 
     // 소셜로그인
+    @Operation(summary = "소셜로그인" , description = "소셜로그인 정보 기반으로 회원을 Upsert 합니다.")
     @PostMapping
     public ResponseEntity<BaseResponse<UserLoginResponse>> socialLogin(@RequestBody @Validated UserLoginRequest request) {
         UserLoginResponse response = userService.createOrUpdateUser(request);
@@ -39,11 +44,14 @@ public class UserController {
     }
 
     // 회원 정보 수정
+    @Operation(summary = "회원 정보 수정" ,
+            description =
+                    "UserPatchRequest를 기반으로 회원정보를 수정합니다. 수정한 유저 정보 기반으로 토큰을 재발급합니다.")
     @PatchMapping
     public ResponseEntity<BaseResponse<UserResponse>> updateUser(
             @RequestBody @Validated UserPatchRequest request,
             @RequestHeader(value = "X-User-Code") String userCode,
-            @CookieValue(value = "deviceCode") String deviceCode,
+            @CookieValue(value = "deviceCode" , required = false) String deviceCode,
             HttpServletResponse response
     ){
             UserResponse user = userService.updateUser(request,userCode);
@@ -53,6 +61,7 @@ public class UserController {
     }
 
     // 회원 목록 조회
+    @Operation(summary = "회원 목록 조회" , description = "전체 회원 목록을 조회합니다.")
     @GetMapping
     public ResponseEntity<BaseResponse<List<UserResponse>>> getAllUsers() {
         List<UserResponse> users = userService.getUserList();
