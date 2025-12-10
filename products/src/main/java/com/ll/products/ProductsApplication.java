2ab69e3b9b7226beb07382d35880977a48df0c76
Kim minseok <80530044+98minseok@users.noreply.github.com>
2025-12-10 15:02:21 +0900

    [feat][171] Swagger 추가 작업 (#172)
    * [feat][171] Gateway 통한 요청 추가
    
    * [feat][171] 모듈별 SwaggerConfig Core SwaggerConfig로 통합
    
    * [feat][171] 커밋 코드 분석 수정

--- products/src/main/java/com/ll/products/ProductsApplication.java
+++ products/src/main/java/com/ll/products/ProductsApplication.java
@@ -1,24 +1,27 @@
 package com.ll.products;
 
+import com.ll.core.config.swagger.SwaggerConfig;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaAuditing
 @EnableScheduling
 @SpringBootApplication
+@Import({SwaggerConfig.class})
 public class ProductsApplication {
     public static void main(String[] args) {
-//        Dotenv dotenv = Dotenv.configure()
-//                .directory("./") // .env 파일 경로 설정
-//                .load();
-//
-//        dotenv.entries().forEach(entry ->
-//                System.setProperty(entry.getKey(), entry.getValue())
-//        );
-//        SpringApplication.run(ProductsApplication.class, args);
+        Dotenv dotenv = Dotenv.configure()
+                .directory("./") // .env 파일 경로 설정
+                .load();
+
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+        );
+        SpringApplication.run(ProductsApplication.class, args);
     }
 
 }
