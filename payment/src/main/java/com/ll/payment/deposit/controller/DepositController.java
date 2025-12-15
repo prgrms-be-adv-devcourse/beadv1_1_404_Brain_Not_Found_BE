828cf32d64aa32718af15df26874aa46acfa6259
Dianuma <dltmdgus1208@gmail.com>
2025-12-15 10:29:13 +0900

    - dev 용 Controller 생성 - prod 용 Controller 에서 필요없는 엔드포인트 삭제 - DepositPaymentServiceImpl 에서 withdrawDeposit -> paymentDeposit 변경

--- payment/src/main/java/com/ll/payment/deposit/controller/DepositController.java
+++ payment/src/main/java/com/ll/payment/deposit/controller/DepositController.java
@@ -4,17 +4,16 @@ import com.ll.core.model.response.BaseResponse;
 import com.ll.core.model.vo.common.DateRange;
 import com.ll.payment.deposit.controller.swagger.*;
 import com.ll.payment.deposit.model.vo.request.DepositDeleteRequest;
-import com.ll.payment.deposit.model.vo.request.DepositTransactionRequest;
 import com.ll.payment.deposit.model.vo.response.DepositDeleteResponse;
 import com.ll.payment.deposit.model.vo.response.DepositHistoryPageResponse;
 import com.ll.payment.deposit.model.vo.response.DepositResponse;
-import com.ll.payment.deposit.model.vo.response.DepositTransactionResponse;
 import com.ll.payment.deposit.service.DepositService;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,6 +22,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/deposits")
+@Profile("prod")
 public class DepositController {
     private final DepositService depositService;
 
@@ -35,64 +35,6 @@ public class DepositController {
 
     ) {
         return BaseResponse.ok(depositService.getDepositByUserCode(userCode));
-    }
-
-    @PostMapping
-    @CreateDepositApiResponse
-    public ResponseEntity<BaseResponse<DepositResponse>> createDeposit(
-            @NotBlank(message = "userCode 는 공백이거나 null일 수 없습니다.")
-            @RequestHeader(value = "X-User-Code")
-            String userCode
-    ) {
-        return BaseResponse.created(depositService.createDeposit(userCode));
-    }
-
-    @PostMapping("/charge")
-    @ChargeDepositApiResponse
-    public ResponseEntity<BaseResponse<DepositTransactionResponse>> chargeDeposit(
-            @NotBlank(message = "userCode 는 공백이거나 null일 수 없습니다.")
-            @RequestHeader(value = "X-User-Code")
-            String userCode,
-            @Parameter(hidden = true)
-            @Valid @RequestBody DepositTransactionRequest request
-    ) {
-        return BaseResponse.ok(depositService.chargeDeposit(userCode, request));
-    }
-
-    @PostMapping("/withdraw")
-    @WithdrawDepositApiResponse
-    public ResponseEntity<BaseResponse<DepositTransactionResponse>> withdrawDeposit(
-            @NotBlank(message = "userCode 는 공백이거나 null일 수 없습니다.")
-            @RequestHeader(value = "X-User-Code")
-            String userCode,
-            @Parameter(hidden = true)
-            @Valid @RequestBody DepositTransactionRequest request
-    ) {
-        return BaseResponse.ok(depositService.withdrawDeposit(userCode, request));
-    }
-
-    @PostMapping("/payment")
-    @PaymentDepositApiResponse
-    public ResponseEntity<BaseResponse<DepositTransactionResponse>> paymentDeposit(
-            @NotBlank(message = "userCode 는 공백이거나 null일 수 없습니다.")
-            @RequestHeader(value = "X-User-Code")
-            String userCode,
-            @Parameter(hidden = true)
-            @Valid @RequestBody DepositTransactionRequest request
-    ) {
-        return BaseResponse.ok(depositService.paymentDeposit(userCode, request));
-    }
-
-    @PostMapping("/refund")
-    @RefundDepositApiResponse
-    public ResponseEntity<BaseResponse<DepositTransactionResponse>> refundDeposit(
-            @NotBlank(message = "userCode 는 공백이거나 null일 수 없습니다.")
-            @RequestHeader(value = "X-User-Code")
-            String userCode,
-            @Parameter(hidden = true)
-            @Valid @RequestBody DepositTransactionRequest request
-    ) {
-        return BaseResponse.ok(depositService.refundDeposit(userCode, request));
     }
 
     @PatchMapping("/close")
