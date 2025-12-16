edf95acf4b1fd82386a9524c387cd3c8f926a6b1
signalzero <117821255+Signalzero94@users.noreply.github.com>
2025-12-16 14:25:08 +0900

    [Docs][220] 상품 모듈 swagger 작성 (#231)
    * [docs][product] 상품 모듈 swagger 작성
    
    * [fix][product] swagger response code 수정
    
    * [fix][product] swagger description 수정
    
    * [fix][product] swagger response fix
    
    ---------
    
    Co-authored-by: Signalzero94 <tlsghdud2222@gmail.com>

--- products/src/main/java/com/ll/products/domain/s3/controller/S3Controller.java
+++ products/src/main/java/com/ll/products/domain/s3/controller/S3Controller.java
@@ -3,10 +3,13 @@ package com.ll.products.domain.s3.controller;
 import com.ll.core.model.response.BaseResponse;
 import com.ll.products.domain.s3.model.dto.response.PresignedUrlResponse;
 import com.ll.products.domain.s3.service.S3Service;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Image", description = "S3 관련 API")
 @RestController
 @RequestMapping("/api/images")
 @RequiredArgsConstructor
@@ -14,7 +17,7 @@ public class S3Controller {
 
     private final S3Service s3Service;
 
-    // 1. presigned url 생성
+    @Operation(summary = "presigned url 생성")
     @GetMapping("/presigned-url")
     public ResponseEntity<BaseResponse<PresignedUrlResponse>> getPresignedUploadUrl(
             @RequestParam("filename") String filename
@@ -22,12 +25,12 @@ public class S3Controller {
         return BaseResponse.ok(s3Service.generatePresignedUploadUrl(filename));
     }
 
-    // 2. 이미지 파일 삭제
-    @DeleteMapping
-    public ResponseEntity<BaseResponse<Void>> deleteImage(
-            @RequestParam("fileKey") String fileKey
-    ) {
-        s3Service.deleteImage(fileKey);
-        return BaseResponse.ok(null);
-    }
+//    @Operation(summary = "imageFile 삭제")
+//    @DeleteMapping
+//    public ResponseEntity<BaseResponse<Void>> deleteImage(
+//            @RequestParam("fileKey") String fileKey
+//    ) {
+//        s3Service.deleteImage(fileKey);
+//        return BaseResponse.ok(null);
+//    }
 }
