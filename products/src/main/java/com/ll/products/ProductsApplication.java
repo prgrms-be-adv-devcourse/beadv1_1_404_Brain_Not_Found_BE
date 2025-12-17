6801e2e96a31e3f611ec283a8868b22c9ada442b
Signalzero94 <tlsghdud2222@gmail.com>
2025-12-17 12:08:46 +0900

    [refactor][product] 사용하지 않는 로직 삭제

--- products/src/main/java/com/ll/products/ProductsApplication.java
+++ products/src/main/java/com/ll/products/ProductsApplication.java
@@ -1,6 +1,7 @@
 package com.ll.products;
 
 import com.ll.core.config.swagger.SwaggerConfig;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
@@ -15,14 +16,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @Import({SwaggerConfig.class})
 public class ProductsApplication {
     public static void main(String[] args) {
-//        Dotenv dotenv = Dotenv.configure()
-//                .directory("./") // .env 파일 경로 설정
-//                .load();
-//
-//        dotenv.entries().forEach(entry ->
-//                System.setProperty(entry.getKey(), entry.getValue())
-//        );
         SpringApplication.run(ProductsApplication.class, args);
     }
-
 }
